3e5897830d6528f909f11a98783a4cf5b6bdf4c0
newancyrille0-sys <newancyrille0@gmail.com>
2025-11-06 22:14:23 +0000

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Bienvenue sur Vitrine Pour Tous</h1>
+      <p>Créez et explorez des vitrines d'entreprises facilement.</p>
+    </div>
+  );
+}
